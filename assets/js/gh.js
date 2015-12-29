54218995c9b7986e61f41eb13b19080a7e0d8680
Austin Vecchio <au.vecchio@gmail.com>
2015-12-28 18:59:42 -0800

    Major update

--- assets/js/gh.js
+++ assets/js/gh.js
@@ -1,45 +1,52 @@
-var repositories = {
+window.Github = function(){
+
+    Github.prototype.function = projects(){
+    };
+
+    Github.prototype.function = index(){
+      var projects = [];
+      for(var i=0; i<repositories.length; i++){
+        var repo;
+        repo.name = repositories[i].id;
+        repo.type = repositories[i].type;
+        $.ajax({
+          url: github_url(repo.name,repo.type),
+          data: {format: 'json'}
+          error: function(){
+            console.log('Error has occured retrieving repository: '+repo.name);
+          }
+          success: function(data){
+            repo.data = data;
+          }
+        });
+        projects.push(repo);
+      }
+      return projects;
+    }
+
+    Github.prototype.function = url(){
+      if(type=="user"){
+        return "https://api.github.com/users/"+id+"/repos"
+      }
+      else{
+        return "https://api.github.com/orgs/"+id+"/repos"
+      }
+    };
+
+    Github.prototype.function = format(){
+      var projects = projects();
+      var bin = [];
+      for(var i=0; i<projects.length; i++){
+
+      }
+    }
+};
+/*
+github.repositories = {
   {id="avecchio",type="user"},
   {id="vortexlaboratory",type="org"},
   {id="binary-labs",type="org"},
   {id="sprocket-games",type="org"},
   {id="power-house",type="org"},
 }
-
-function github_url(id,type){
-  if(type=="user"){
-    return "https://api.github.com/users/"+id+"/repos"
-  }
-  else{
-    return "https://api.github.com/orgs/"+id+"/repos"
-  }
-}
-
-function projects(){
-  var projects = [];
-  for(var i=0; i<repositories.length; i++){
-    var repo;
-    repo.name = repositories[i].id;
-    repo.type = repositories[i].type;
-    $.ajax({
-      url: github_url(repo.name,repo.type),
-      data: {format: 'json'}
-      error: function(){
-        console.log('Error has occured retrieving repository: '+repo.name);
-      }
-      success: function(data){
-        repo.data = data;
-      }
-    });
-    projects.push(repo);
-  }
-  return projects;
-}
-
-function ghformat(){
-  var projects = projects();
-  var bin = [];
-  for(var i=0; i<projects.length; i++){
-
-  }
-}
+*/
